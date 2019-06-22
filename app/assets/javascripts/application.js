19d84c622fe0475d6fd431bc98db28fd79292b44
vincentbwalker <vincethewalker@gmail.com>
2019-06-22 19:52:23 +0000

    Added Web Sockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,7 +23,23 @@ function scrollToBottom(){
     $('#messages').scrollTop($('#messages')[0].scrollHeight);
   }
 }
+function submitMessage(event){
+  event.preventDefault();
+  $('#new_message').submit();
+}
+$(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+  if (event.keyCode === 13) {
+    submitMessage(event);
+}
+});
 
-$(document).ready(function() {
+$(document).on('click', '[data-send~=message]', function(event) {
+  submitMessage(event);
+});
+
+$(document).on('turbolinks:load', function() {
+  $("#new_message").on("ajax:complete", function(e, data, status) {
+    $('#message_content').val('');
+  });
   scrollToBottom();
 });
